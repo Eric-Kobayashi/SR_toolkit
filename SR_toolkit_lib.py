3a09619fcde7cfad6752b3e1863280a34e3f8256
Eric Kobayashi <yz520@cam.ac.uk>
2019-01-17 13:29:15 +0000

    Upload SR toolkit v3_2

--- SR_toolkit_lib.py
+++ SR_toolkit_lib.py
@@ -1,33 +1,14 @@
 '''
-SR_toolkit_v3_1
+SR_toolkit_v3_2
 
 Issued: 20-Dec-2018
 @author: Eric Kobayashi
 
-Major changes over v3:
-    1. Incorporate ImageJ GDSC SMLM peak fit and the brightness-based fiducial
-    correction code into the analysis. Re-organise the main run_fit function
-    so it workflows the ImageJ part and the python part. The ImageJ part is
-    'lazy'--- it only runs if a previous analysis using the same parameters
-    is not found in the same root path. Notice that if only fitresults file are 
-    available (images are not available), this script will not run properly. 
-    Please use SR_toolkit_v3_0 for that. 
-    
-    2. Trimming the frame to a certain frame_number is supported. This allows a
-    certain number of frames to be processed. 
-    
-    3. Cluster analysis measurement can be turned off. This allows only GDSC SMLM
-    to run and therefore a quick check of the localisation number.
-    
-    4. A symbolic link to the original image stack can be generated in the 
-    analysis folder. This feature needs python to be run in administrator mode.
-     
-Some minor changes:    
-    1. All the analysis now are by default copied. This means no analysis files
-    will be appended to the raw image folder. 
-    2. Use JSON to output the logging information rather than xml. More human
-    readable.
-    3. Fix some bugs in the library.
+Changes over v3_1:
+    1. Add compatibility with v3_0, i.e. run cluster fit only.
+    2. Add fiducial markers removal.
+    3. Save fiducial marker number in the summary.
+    4. Fix some bugs in the library.
     
 Potential features update:
     2. Integrate more ImageJ analysis including GDSC SMLM, background correction, 
@@ -36,7 +17,6 @@ Potential features update:
     rather than localisations can be more precise.
     4. Use burst analysis for fiducial correction. (need to check papers)
     5. Length measurement is slow. Could be improved.
-    6. Integrate sqlite to pack the outputs into database automatically. (ongoing)
     
 '''
 
@@ -45,6 +25,8 @@ from shutil import copyfile, SameFileError
 import sys
 from datetime import datetime
 from collections import OrderedDict
+import numpy as np
+import pandas as pd
 from pandas import DataFrame as DF
 import json
 from copy import deepcopy
@@ -149,13 +131,40 @@ class Analysis(object):
         fit_name = d['fit_name']
         self._log_fit_info(fit_name, **kwargs)
         self.json_log['ImageJ_log'] =  d['ImageJ_log']
+         
         error_log = [(img, log) for img, log in d['ImageJ_log'].items() 
                     if 'Error' in ''.join(log.values())]
         if len(error_log) > 0:
             for i, l in error_log:
                 warnings.warn("{}:\n\tError in ImageJ {} analysis, message: "
                 "{}.\n".format(i, l.keys()[0], l[0]), UserWarning)
+        
         return fit_name
+        
+    def _remove_fid(self, b, s):
+        self._search_fitresults()
+        List_of_corrected = []
+        for fr in self.fitpathlist:
+            file_dir = os.path.dirname(fr)
+            if 'Fiducials.txt' in os.listdir(file_dir) and \
+            'FitResults_FeuRemoved.txt' not in os.listdir(file_dir):
+                fid_file = os.path.join(file_dir, 'Fiducials.txt')
+                fit_res = os.path.join(file_dir, 'FitResults_Corrected.txt')
+                List_of_corrected.append((fit_res, fid_file))
+        
+        for fit_res, fid_file in List_of_corrected:
+            df = pd.read_table(fit_res)
+            feus = pd.read_table(fid_file).values
+            for feu in feus:
+                feu = np.array(feu)
+                feu = np.append(feu[0:2] - [s,s], feu[0:2] + [s,s])
+                df = df[(df.X<feu[0]) | (df.Y<feu[1]) | (df.X>feu[2]) | (df.Y>feu[3])]
+                df = df[df.origValue<2*b]
+            df = df.reset_index(drop=True)
+            df.to_csv(os.path.join(os.path.dirname(fit_res), 
+            'FitResults_FeuRemoved.txt'), index = False, sep = '\t')
+        
+        self.fit_name = 'FitResults_FeuRemoved.txt'
         
     def _create_symlinks(self):
         assert ctypes.windll.shell32.IsUserAnAdmin(), 'You need Admin'\
@@ -172,8 +181,7 @@ class Analysis(object):
                 src = path_f.read()
             link = os.path.join(r, 'Raw_image.lnk')
             os.symlink(src, link)
-            
-                  
+                      
     def run_fit(self, image_condition=None, verbose=True, **kwargs):
         '''
         Run fit analysis on tiff image stacks acquired by super-resolution imaging. 
@@ -184,40 +192,45 @@ class Analysis(object):
         '''
 
         # Check if the matched fitresults exists
-
-        if verbose:
-            sys.stdout.write('Looking for tiff image files meeting '\
-            'defined image condition in \npath: {}...\n'.format(self.path))  
-        self._search_images(image_condition)
-        assert self.n_img > 0, 'No image found!'
-        if verbose:
-            sys.stdout.write("{} images found.\n".format(self.n_img))
-
-        this_peak_fit_info = self._peak_fit_info(**kwargs)
-        existing_peak_fit = self._existing_peak_fit()
-        self.fitresults_folder = None
-        for peak_fit_location, peak_fit_dict in existing_peak_fit.items():
-            peak_fit_info = deepcopy(peak_fit_dict)
-            del peak_fit_info['fit_name']
-            if this_peak_fit_info == peak_fit_info:
-                self.fit_name = peak_fit_dict['fit_name']
-                self.fitresults_folder = peak_fit_location
-                break
-        if not self.fitresults_folder:
+        if kwargs['GDSC_SMLM_peak_fit']:
             if verbose:
-                sys.stdout.write('No existing GDSC SMLM fit results found, '
-                 'start running peak fit in ImageJ...\n') 
-            self.fit_name = self.peak_fit(**kwargs)
-            self.fitresults_folder = self.results_dir
+                sys.stdout.write('Looking for tiff image files meeting '\
+                'defined image condition in \npath: {}...\n'.format(self.path))  
+            self._search_images(image_condition)
+            assert self.n_img > 0, 'No image found!'
+            if verbose:
+                sys.stdout.write("{} images found.\n".format(self.n_img))
+    
+            this_peak_fit_info = self._peak_fit_info(**kwargs)
+            existing_peak_fit = self._existing_peak_fit()
+            self.fitresults_folder = None
+            for peak_fit_location, peak_fit_dict in existing_peak_fit.items():
+                peak_fit_info = deepcopy(peak_fit_dict)
+                del peak_fit_info['fit_name']
+                if this_peak_fit_info == peak_fit_info:
+                    self.fit_name = peak_fit_dict['fit_name']
+                    self.fitresults_folder = peak_fit_location
+                    break
+            if not self.fitresults_folder:
+                if verbose:
+                    sys.stdout.write('No existing GDSC SMLM fit results found, '
+                    'start running peak fit in ImageJ...\n') 
+                self.fit_name = self.peak_fit(**kwargs)
+                self.fitresults_folder = self.results_dir
+            else:
+                if verbose:
+                    sys.stdout.write('Existing GDSC SMLM fit results found in path:\n'
+                    '{}\n'.format(self.fitresults_folder)) 
         else:
             if verbose:
-                sys.stdout.write('Existing GDSC SMLM fit results found in path:\n'
-                 '{}\n'.format(self.fitresults_folder)) 
-
+                sys.stdout.write('GDSC SMLM peak fit turned off.')
+            self.fit_name = 'NotApplicable'
+            self.fitresults_folder = self.path
+            
         self.cluster_fit(verbose=verbose, **kwargs)               
         self._logging(**kwargs)    
-                             
-        if kwargs['create_symlink_for_images']:
+    
+        if kwargs['GDSC_SMLM_peak_fit'] and kwargs['create_symlink_for_images']:
             self._create_symlinks()
         
         sys.stdout.write("\nTime: {} Analysis finished!\n\n".format(
@@ -235,13 +248,23 @@ class Analysis(object):
             save_histogram=True, save_GDSC_header_file = True
         '''
                     
+        assert kwargs['GDSC_SMLM_peak_fit'] or kwargs['fitresults_file_name'] != 'default', \
+        'Please enter fitresults_file_name'
         if kwargs['fitresults_file_name'] != 'default':
             self.fit_name = kwargs['fitresults_file_name']
+        else:
+            # Remove fiducials
+            if kwargs['fiducial_correction']['run']:
+                self._remove_fid(kwargs['fiducial_correction']['fid_brightness'],
+                kwargs['fiducial_correction']['fid_size'])
+                
+        self._search_fitresults()
+        assert self.n_fit > 0, 'No fit results files found!'
+
         if verbose:
             sys.stdout.write("Looking for super-res fit result files '{}' in \npath: {}\n".format(
                 self.fit_name, self.fitresults_folder))  
-        self._search_fitresults()
-        assert self.n_fit > 0, 'No fit results files found!'
+            
         self.resultslist = [SR_fit(fitpath) for fitpath in self.fitpathlist]
         # OrderedDefaultDict is used here. (defined below)
         self.to_summary = OrderedDefaultDict(list) 
@@ -255,16 +278,18 @@ class Analysis(object):
             new_dir = fit.root.replace(self.fitresults_folder, self.results_dir)
             if not os.path.isdir(new_dir):
                 os.makedirs(new_dir)
-            for roots, dirs, files in os.walk(fit.root):
-                for f in files:
-                    if f.endswith('.path.txt'):
-                        f_root = roots
-                        break
-            try:
-                copyfile(os.path.join(f_root, f), os.path.join(new_dir, f))
-            except SameFileError:
-                pass
-            
+                
+            if kwargs['GDSC_SMLM_peak_fit']:
+                for roots, dirs, files in os.walk(fit.root):
+                    for f in files:
+                        if f.endswith('.path.txt'):
+                            f_root = roots
+                            break
+                    try:
+                        copyfile(os.path.join(f_root, f), os.path.join(new_dir, f))
+                    except SameFileError:
+                        pass
+    
             fit.input_parameters(results_dir=new_dir, **kwargs)
             fit_ID = 'T'+ self.timestring + '_' + fit.path # This serves as the
                         # primary key for the summary database
@@ -272,6 +297,12 @@ class Analysis(object):
             self.to_summary['Analysis_ID'].append(fit.fit_ID)
             self.to_summary['Raw_loc_number'].append(fit.loc_num())
             self.to_summary['Frame_number'].append(fit.framenum)
+            if 'Fiducials.txt' in os.listdir(fit.root):
+                fid_file = os.path.join(fit.root, 'Fiducials.txt')
+                feus = pd.read_table(fid_file).values
+                self.to_summary['Fiducials_number'].append(len(feus))
+            else:
+                self.to_summary['Fiducials_number'].append(0)
             
         if kwargs['cluster_analysis_measurement']:
             # Run cluster analysis
@@ -444,6 +475,13 @@ class Analysis(object):
         d['fitresults_source'] = self.fitresults_folder
         d['fitresults_name'] = self.fit_name
         self.json_log.update(d)
+        if not kwargs['GDSC_SMLM_peak_fit']:
+            self.json_log['trim_track'] = 'NotApplicable'
+            self.json_log['signal_strength'] = 'NotApplicable'
+            self.json_log['precision'] = 'NotApplicable'
+            self.json_log['min_photons'] = 'NotApplicable'
+            self.json_log['fiducial_correction'] = 'NotApplicable'
+            
         with open(os.path.join(self.results_dir, "log.json"), 'w') as log:
             json.dump(self.json_log, log, indent=2)
         
@@ -510,8 +548,12 @@ class OrderedDefaultDict(OrderedDict):
                                                OrderedDict.__repr__(self))
                                                
 if __name__ == '__main__':
-    directory = r"C:\Users\yz520\Desktop\OneDrive - University Of Cambridge\igorplay\cell_training_set"
-    image_condition = lambda img: img.endswith('_trimmed.tif')  # Change conditions to search for imagestacks
+    '''
+    Input variables
+    
+    '''
+    directory = r"C:\Users\yz520\Desktop\OneDrive - University Of Cambridge\igorplay\feudicial"
+    image_condition = lambda img: img.endswith('_561.tif')  # Change conditions to search for imagestacks
     input_dict = {
     # ==== Parameters for all analysis ====
     'pixel_size': 107 , # nm
@@ -520,21 +562,22 @@ if __name__ == '__main__':
     'create_symlink_for_images': False, # Needs admin mode
     
     # ==== Parameters for GDSC SMLM fitting ====
-    'trim_track': {'run': True, 'frame_number': 4000, 'from_end': True}, # trim the stack to the required frame number, from_end controls trim from end or beginning 
-    'signal_strength': 150, 
+    'GDSC_SMLM_peak_fit': True, # If False, the parameters for GDSC SMLM fitting will be ignored, only cluster_analysis will be run
+    'trim_track': {'run': False, 'frame_number': 4000, 'from_end': True}, # trim the stack to the required frame number, from_end controls trim from end or beginning 
+    'signal_strength': 0, 
     'precision': 20, # nm
     'min_photons': 0,
-    'fiducial_correction': {'run':False, 'fid_brightness':20000},
+    'fiducial_correction': {'run':True, 'fid_brightness':20000, 'fid_size': 6},
     
     # ==== Parameters for cluster analysis and measurements ====
-    'cluster_analysis_measurement': False, # If False, only GDSC fitting will be run
+    'cluster_analysis_measurement': True, # If False, only GDSC fitting will be run
     'fitresults_file_name': 'default', # default: 'FitResults.txt' or 'FitResults_Corrected.txt' if fiducial corrected
     'DBSCAN_eps': 100 , # nm, not pixels!
-    'DBSCAN_min_samples': 1 ,
-    'burst_filter': {'run':True, 'fill_gap':50, 'min_burst':3} ,  # filter out the aggregates with less than min_bursts
-    'burst_analysis': {'run':True, 'fill_gap':5, 'remove_single':True} ,  # analyse the burst number, burst length, dark length of the aggregates
-    'length_measure': True ,
-    'eccentricity_measure': True ,
+    'DBSCAN_min_samples': 5 ,
+    'burst_filter': {'run':False, 'fill_gap':50, 'min_burst':3} ,  # filter out the aggregates with less than min_bursts
+    'burst_analysis': {'run':False, 'fill_gap':5, 'remove_single':True} ,  # analyse the burst number, burst length, dark length of the aggregates
+    'length_measure': False ,
+    'eccentricity_measure': False ,
     'convexhull_measure': True , # measure the area of the cluster
     'save_GDSC_header_file': True , # save GSDC header file for rendering, will consider remove since rendering will be incorporated in this code
     'save_histogram':True,
