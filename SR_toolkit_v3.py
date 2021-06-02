168d859a2d76d684fce730e7e48a1211ccb7445c
Eric-Kobayashi <33022148+Eric-Kobayashi@users.noreply.github.com>
2021-06-02 17:42:34 +0100

    Update SR_toolkit_v3.py

--- SR_toolkit_v3.py
+++ SR_toolkit_v3.py
@@ -37,20 +37,20 @@ def mainfunc():
     'fiducial_correction': {'run':False, 'correction_method':'auto_fid', # correction_method: 'auto_fid', 'fid_file', 'drift', 'corr' 
     'bin_size': 20, 'segpara':500, # parameters for 'corr' - correlation correction, bin_size in nm, segpara in frames
     'fid_brightness':10000, 'fid_size': 6, 'fid_last_time':500, # parameters for 'auto_fid' - automatically finding fiducials, fid_last_time in frames
-    'smoothing_para':0.25, 'limit_smoothing':True},  # smoothing_para and limit_smoothing controls the correction smoothing. If smoothing fails try setting limit_smotthing to False
+    'smoothing_para':0.25, 'limit_smoothing':True},  
 
     
     # ==== Parameters for cluster analysis and measurements ====
-    'fitresults_file_name': 'default', # default: 'FitResults.txt' or 'FitResults_FeuRemoved.txt' if fiducial corrected
+    'fitresults_file_name': 'default', 
     'temporal_grouping': {'run': True, 'dThresh':15, 'min_loc':2, 'max_mol_area':float('inf'),
      'tThresh':101, 'min_frame':2, 'min_burst':2, 'min_on_prop':0.0},
      # dThresh - nm, max_mol_area - nm2, tThresh - ms
-    'cluster_analysis': {'run': True, 'cluster_subject':'burst', # burst, mol, loc
+    'cluster_analysis': {'run': True, 'cluster_subject':'burst', # burst, loc
     'DBSCAN_eps_nm':75, 'DBSCAN_min_samples':2},
-    'length_measure': {'run':True, 'algorithm':'close', 'sigma':2}, # algorithm: blur or close; if blur, sigma is the gaussian sigma; if close, sigma is the closing square size
+    'length_measure': {'run':True, 'algorithm':'close', 'sigma':2}, 
     'eccentricity_measure': True ,
-    'convexhull_measure': True , # measure the area of the cluster
-    'Rendering_SR': 'loc_pre' , # loc, loc_pre, or False, rendering clustered SR images in imageJ
+    'convexhull_measure': True , 
+    'Rendering_SR': 'loc_pre' , # loc, loc_pre, or False
     'save_histogram':True,
     }
 
